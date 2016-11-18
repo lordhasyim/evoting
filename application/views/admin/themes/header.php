f0dacf5b9d30876861bd4252237f9145232d604d
hasyimgit <lordhasyim@gmail.com>
2016-11-18 18:37:52 +0700

    adding assets file

---
+++ application/views/admin/themes/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title> Simila </title>
+
+    <!-- Core CSS - Include with every page -->
+    <link rel="stylesheet" href="<?php echo base_url("assets/bootstrap/css/bootstrap.css"); ?>"/>
+    <script type="text/javascript" src="<?php echo base_url("assets/bootstrap/js/bootstrap.js"); ?>"></script>
+    <script type="text/javascript" src="<?php echo base_url("assets/bootstrap/jquery-1.10.2.js"); ?>"></script>
+    <link rel="stylesheet" href="<?php echo base_url("assets/bootstrap/loginstyle.css"); ?>"/>
+    <link rel="stylesheet" href="<?php echo base_url("assets/admin/timeline.css"); ?>"/>
+    <link rel="stylesheet" href="<?php echo base_url("assets/admin/sb-admin.css"); ?>"/>
+    <link rel="stylesheet" href="<?php echo base_url("assets/admin/sb-admin.js"); ?>"/>
+    <link rel="stylesheet" href="<?php echo base_url("assets/admin/font-awesome/css/font-awesome.css"); ?>"/>
+
+    <!-- Page-Level Plugin CSS - Dashboard -->
+    <!--<link href="css/plugins/morris/morris-0.4.3.min.css" rel="stylesheet">
+    <link href="css/plugins/timeline/timeline.css" rel="stylesheet">
+-->
+</head>
