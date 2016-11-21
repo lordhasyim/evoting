cb4d6240caf18966e24cdb0f97cba4f6eb60893b
hasyimgit <lordhasyim@gmail.com>
2016-11-21 17:56:32 +0700

    fixing menu top menu

--- application/views/admin/themes/footer.php
+++ application/views/admin/themes/footer.php
@@ -13,7 +13,9 @@
 <!-- /#wrapper -->
 
 <!-- Core Scripts - Include with every page -->
-
+<script type="text/javascript" src="<?php echo base_url("assets/bootstrap/jquery-1.10.2.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/bootstrap/js/bootstrap.js"); ?>"></script>
+<scrript type="text/javascript" src="<?php echo base_url("assets/admin/sb-admin.js"); ?>"></scrript>
 </body>
 
 </html>
--- application/views/admin/themes/header.php
+++ application/views/admin/themes/header.php
@@ -10,12 +10,9 @@
 
     <!-- Core CSS - Include with every page -->
     <link rel="stylesheet" href="<?php echo base_url("assets/bootstrap/css/bootstrap.css"); ?>"/>
-    <script type="text/javascript" src="<?php echo base_url("assets/bootstrap/js/bootstrap.js"); ?>"></script>
-    <script type="text/javascript" src="<?php echo base_url("assets/bootstrap/jquery-1.10.2.js"); ?>"></script>
     <link rel="stylesheet" href="<?php echo base_url("assets/bootstrap/loginstyle.css"); ?>"/>
     <link rel="stylesheet" href="<?php echo base_url("assets/admin/timeline.css"); ?>"/>
     <link rel="stylesheet" href="<?php echo base_url("assets/admin/sb-admin.css"); ?>"/>
-    <link rel="stylesheet" href="<?php echo base_url("assets/admin/sb-admin.js"); ?>"/>
     <link rel="stylesheet" href="<?php echo base_url("assets/admin/font-awesome/css/font-awesome.css"); ?>"/>
 
     <!-- Page-Level Plugin CSS - Dashboard -->
